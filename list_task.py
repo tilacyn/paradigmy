02c1e07e85e1f17f052918c483573a0b3c629a36
tilacyn <tilacyn@yandex.ru>
2016-09-24 22:27:19 +0300

    Update list_task.py

--- list_task.py
+++ list_task.py
@@ -1,14 +1,16 @@
 def remove_adjacent(s):
-    s1[]
-    i=1
-    s1.append(s[0])
-    while (i<len(s)):
-        if(s[i]!=s[i-1]):
-            s1.append(s[i])
-        i=i+1
+    if(s==[]):
+        return 
+    else:
+        s1=[]
+        s1.append(s[0])
+        for i in range (1 , len(s)):
+            if(s[i]!=s[i-1]):
+                s1.append(s[i])
+            i=i+1
             
             
-    return s1
+        return s1
 
 
 def linear_merge(lst1, lst2):
@@ -23,11 +25,9 @@ def linear_merge(lst1, lst2):
             s.append(lst1[i])
             i=i+1
             
-    if(i==len(lst1)):
-        s.extend(lst2[j:])
-    else:
-        s.extend(lst1[i:])
+   
+    s.extend(lst2[j:])
+    
+    s.extend(lst1[i:])
             
     return s
-
-
