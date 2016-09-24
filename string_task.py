a3958881befcd0d507aaa41c633792659ab60455
tilacyn <tilacyn@yandex.ru>
2016-09-24 22:34:41 +0300

    Update string_task.py

--- string_task.py
+++ string_task.py
@@ -1,4 +1,3 @@
-
 def verbing(s):
   if(len(s)>=3): 
     if s[-3:]=='ing':
@@ -14,19 +13,17 @@ def verbing(s):
 def not_bad(s):
     x=s.find('not')
     y=s.find('bad')
-    if(x>=0 and x<y)
+    if(x>=0 and x<y):
       return s[:x]+'good'+s[y+3:]
-    else
+    else:
       return s
 
 
 def front_back(a,b):
   x=len(a)
   y=len(b)
-  p=x//2+x%2+1
-  r=y//2+y%2+1
-  s[]
+  p=x//2+x%2
+  r=y//2+y%2
+  s=[]
   s=a[:p]+b[:r]+a[p:]+b[r:]
   return s
-
-  
