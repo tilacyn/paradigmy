1c2ebddacecb22e84f6248aca2feb7e9af61b836
tilacyn <tilacyn@yandex.ru>
2016-10-03 23:57:59 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import hashlib
+
+
+def hashfile(filename):
+    h = hashlib.md5()
+    with open(filename, "rb") as fd:
+        h.update(fd.read())
+    return h.hexdigest()
+
+
+def dublicate(Directory):
+    dic = {}
+    for path in os.walk(Directory):
+        for files in path[2]:
+            h = hashfile(path[0]+'\\'+files)
+            if files[0] != '.' and files[0] != '~':
+                if dic.get(h):
+                    dic[h].extend([files])
+                else:
+                    dic[h] = [files]
+    return dic
+
+Directory = input()
+
+dic = dublicate(Directory)
+for key in dic:
+    if len(dic[key]) > 1:
+        print(*dic[key], sep=':')
