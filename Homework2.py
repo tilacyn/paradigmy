e9b626747df6946f92ecd5c86538752c06f351de
tilacyn <tilacyn@yandex.ru>
2016-10-05 23:55:13 +0300

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -11,11 +11,11 @@ def hash_file(file_name):
     return h.hexdigest()
 
 
-def search_for_duplicates(directory):
+def search_for_duplicates(global_directory):
     hash_to_files = collections.defaultdict(list)
-    for directory_with_f, _, files in os.walk(directory):
+    for path_to_dir_with_f, _, files in os.walk(global_directory):
             for f in files:
-                absolute_path = os.path.abspath(os.path.join(directory_with_f, f))
+                absolute_path = os.path.abspath(os.path.join(path_to_dir_with_f, f))
                 if not f.startswith(('.', '~')) and not os.path.islink(absolute_path):
                     h = hash_file(absolute_path)
                     hash_to_files[h].append(absolute_path)
