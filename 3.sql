130a2b59d390c4990876cb31e85bb44e08975222
tilacyn <tilacyn@yandex.ru>
2016-12-19 15:35:00 +0300

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,2 +1,2 @@
 SELECT City.Name FROM City JOIN Capital ON Capital.CityId = City.Id
-JOIN Country ON Capital.CountryCode = Country.Code WHERE Country.Name == 'Malaysia';
+JOIN Country ON Capital.CountryCode = Country.Code WHERE Country.Name = 'Malaysia';
