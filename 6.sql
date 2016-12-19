31f6c9fe4b518b46ddd28c3698b61c5c9834dc85
tilacyn <tilacyn@yandex.ru>
2016-12-19 17:48:55 +0300

    Update 6.sql

--- 6.sql
+++ 6.sql
@@ -1,3 +1,3 @@
 SELECT City.Name, City.Population AS City_Population, Country.Population AS Country_Population
 FROM City JOIN Country ON Country.Code = City.CountryCode
-ORDER BY (CAST (City.Population AS DOUBLE) / CAST(Country.Population AS DOUBLE)) DESC;
+ORDER BY (CAST (City.Population AS DOUBLE) / CAST(Country.Population AS DOUBLE)) DESC LIMIT 20;
