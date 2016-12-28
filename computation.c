3c02bd67db3c51348a5c73bf6d640f91617ce1c9
tilacyn <tilacyn@yandex.ru>
2016-12-28 10:47:35 +0300

    Update computation.c

--- computation.c
+++ computation.c
@@ -4,13 +4,22 @@
     
 void thpool_submit_computation(struct ThreadPool *pool, struct Computation *computation, OnComputationComplete on_complete, void* on_complete_args){
 	computation->task.f = computation->f;
-	computation->task.arg = computation->args;
+	computation->task.arg = computation->arg;
 	computation->on_complete = on_complete;
 	computation->on_complete_args = on_complete_args;
 	computation->is_completed = 0;
 	pthread_mutex_init(&computation->mutex, NULL);
 	pthread_cond_init(&computation->cond, NULL);
 	thpool_submit(pool, &computation->task);
+}
+
+void thpool_complete_computation(struct Computation *computation){
+    pthread_mutex_lock(&computation->mutex);
+	if(computation->on_complete)
+		computation->on_complete(computation->on_complete_args);
+	pthread_cond_signal(&computation->cond);
+	computation->is_completed = 1;
+	pthread_mutex_unlock(&computation->mutex);
 }
 
 void thpool_wait_computation(struct Computation *computation){
@@ -21,13 +30,4 @@ void thpool_wait_computation(struct Computation *computation){
 	pthread_cond_destroy(&computation->cond);
 	pthread_mutex_destroy(&computation->mutex);
 	thpool_wait(&computation->task);
-}
-
-void thpool_complete_computation(struct Computation *computation){
-    	pthread_mutex_lock(&computation->mutex);
-	if(computation->on_complete)
-		computation->on_complete(computation->on_complete_args);
-	pthread_cond_signal(&computation->cond);
-	computation->is_completed = 1;
-	pthread_mutex_unlock(&computation->mutex);
 }
